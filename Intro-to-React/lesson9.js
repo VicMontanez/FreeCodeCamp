aca5235dfde6f33c13a8bdd50ff3a72f012e57ba
Victor Montanez <victormontanez.student@careerdevs.com>
2019-05-29 20:31:07 -0400

    lesson9.js

--- Intro-to-React/lesson9.js
+++ Intro-to-React/lesson9.js
@@ -16,6 +16,7 @@ class ParentComponent extends React.Component {
       <div>
         <h1>I am the parent</h1>
         { /* change code below this line */ }
+          
           <ChildComponent />
 
         { /* change code above this line */ }
