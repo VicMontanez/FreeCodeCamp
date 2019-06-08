ad5641d4ec36004331cddb682204817a66b60c00
Victor Montanez <victormontanez.student@careerdevs.com>
2019-06-07 21:56:16 -0400

    lesson4.js

--- Basic-Algorithm-Scripting/lesson4.js
+++ Basic-Algorithm-Scripting/lesson4.js
@@ -1,13 +1,12 @@
 function findLongestWordLength(str) {
-  var words = str.split(' ');
-  var maxLength = 0;
+var words = str.split(' ');
+var wordLength = 0
 
-  for (var i = 0; i < words.length; i++) {
-    if (words[i].length > maxLength) {
-      maxLength = words[i].length;
-    }
+for (var i = 0; i < words.length; i++) {
+  if (words[i].length > wordLength) {
+      wordLength = words[i].length;
   }
-
-  return maxLength;
 }
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+return wordLength
+}
