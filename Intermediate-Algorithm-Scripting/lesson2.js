8c5377d9599003b862712a9c6d42f33a9d88f8ae
VicMontanez <v_montanez@hotmail.com>
2019-06-13 20:36:40 -0400

    lesson2.js

--- Intermediate-Algorithm-Scripting/lesson2.js
+++ Intermediate-Algorithm-Scripting/lesson2.js
@@ -1,8 +1,25 @@
-//lesson 2
-function reverseString(str) {
-  return str.split('').reverse().join('');
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  // Same, same; but different.
+
+  //Iterate through arr1
+  for (let i = 0; i < arr1.length; i++) {
+    //Check if Element is in arr2
+    if (arr2.indexOf(arr1[i]) == -1) {
+       //Store the element in newArr
+      newArr.push(arr1[i])
+    }
+  }
+
+  //Iterate through arr2
+  for (let i =0; i < arr2.length; i++) {
+  //Check if the element is in arr1
+  if (arr1.indexOf(arr2[i]) == -1) {
+  //Store the element in newArr
+    newArr.push(arr2[i])
+   }
+  }
+  return newArr;
 }
 
-reverseString("hello");
-reverseString("Howdy");
-reverseString("Greetings from Earth");
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
