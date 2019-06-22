a7f2ad454851441e747ef08e6f54cf3dd3e58e44
VicMontanez <v_montanez@hotmail.com>
2019-06-21 21:55:10 -0400

    lesson12.js

--- Basic-Algorithm-Scripting/lesson12.js
+++ Basic-Algorithm-Scripting/lesson12.js
@@ -2,7 +2,7 @@
 function frankenSplice(arr1, arr2, n) {
     // It's alive. It's alive!
     var newArr = [...arr2]
-    var arr3 = newArr.splice(n, 0, ...arr1)
+    newArr.splice(n, 0, ...arr1)
    
   console.log(arr2)
    return newArr
