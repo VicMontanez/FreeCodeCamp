9e701dc484fa46a487a7886f4f414ab4d8eeae0a
VicMontanez <v_montanez@hotmail.com>
2019-06-21 21:51:02 -0400

    lesson12.js

---
+++ Basic-Algorithm-Scripting/lesson12.js
@@ -0,0 +1,12 @@
+function frankenSplice(arr1, arr2, n) {
+    // It's alive. It's alive!
+    var newArr = [...arr2]
+    var arr3 = newArr.splice(n, 0, ...arr1)
+   
+  console.log(arr2)
+   return newArr
+  
+    ;
+  }
+  
+  frankenSplice([1, 2, 3], [4, 5, 6], 1);
