f12efd768e9883268eeb621022bb8985bac2bc2a
VicMontanez <v_montanez@hotmail.com>
2019-06-22 23:42:46 -0400

    lesson14.js

---
+++ Basic-Algorithm-Scripting/lesson14.js
@@ -0,0 +1,9 @@
+function getIndexToIns(arr, num) {
+    // Find my place in this sorted array.
+    var newArr = arr.concat(num)
+    
+   var arr2 = newArr.sort((a, b) => a - b).indexOf(num)
+    return arr2;
+  }
+  
+  getIndexToIns([40, 60], 50);
