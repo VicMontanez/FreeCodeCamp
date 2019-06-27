779d31a56922c1fa5b20a4d59adcb82fa755c039
VicMontanez <v_montanez@hotmail.com>
2019-06-26 20:46:23 -0400

    lesson15.js

---
+++ Basic-Algorithm-Scripting/lesson15.js
@@ -0,0 +1,12 @@
+function mutation(arr) {
+    var arr3 = arr[1].toLowerCase();
+    var newArray = arr[0].toLowerCase();
+    for (var i=0;i<arr3.length;i++) {
+      if (newArray.indexOf(arr3[i]) < 0)
+        return false;
+    }
+    return true;
+   }
+  
+  
+  mutation(["hello", "hey"]);
