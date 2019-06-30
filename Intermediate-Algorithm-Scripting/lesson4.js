8e1f6ffca705a317be43b84555ed41cbb8034856
VicMontanez <v_montanez@hotmail.com>
2019-06-30 14:55:41 -0400

    lesson4.js

---
+++ Intermediate-Algorithm-Scripting/lesson4.js
@@ -0,0 +1,19 @@
+function whatIsInAName(collection, source) {
+    // What's in a name?
+    var arr = [];
+    // Only change code below this line
+    var Keyword = Object.keys(source);
+  
+    return collection.filter(function (obj) {
+      return Keyword.every(function (word) {
+        return obj.hasOwnProperty(word) && obj[word] === source[word];
+      });
+    });
+  
+  
+    
+    // Only change code above this line
+    return arr;
+  }
+  
+  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
