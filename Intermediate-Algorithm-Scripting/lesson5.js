f07a30616fdebade94fa061e24a2d65cf9e9e61a
VicMontanez <v_montanez@hotmail.com>
2019-07-01 18:40:01 -0400

    lesson5.js

--- Intermediate-Algorithm-Scripting/lesson5.js
+++ Intermediate-Algorithm-Scripting/lesson5.js
@@ -1,1 +1,11 @@
 //lesson 5
+function spinalCase(str) {
+    // "It's such a fine line between stupid, and clever."
+    // --David St. Hubbins
+  var spaces = /\s+|_+/g;
+  var newStr = str.replace(/([a-z])([A-Z])/g, '$1 $2')
+  
+    return newStr.replace(spaces, '-').toLowerCase()
+  }
+  
+  spinalCase('This Is Spinal Tap');
