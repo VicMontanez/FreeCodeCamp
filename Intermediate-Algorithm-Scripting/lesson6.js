bb9786aeeff1727e87b222dd25643cac22702d93
VicMontanez <v_montanez@hotmail.com>
2019-07-10 22:32:05 -0400

    lesson6

---
+++ Intermediate-Algorithm-Scripting/lesson6.js
@@ -0,0 +1,18 @@
+function translatePigLatin(str) {
+    let firstWord=str[0];
+    let vowels=/[aeiou]/gi;
+    let newWord="";
+    if(!str.match(vowels)){
+      return str + 'ay';
+    } else if(firstWord.match(vowels)){
+       newWord= str+'way';
+  
+    }else{
+      let indexOfVowel=str.indexOf(str.match(vowels)[0]);
+      newWord=str.substr(indexOfVowel)+str.substr(0,indexOfVowel)+'ay';
+    }
+    return newWord;
+  }
+  
+  
+  translatePigLatin("consonant");
