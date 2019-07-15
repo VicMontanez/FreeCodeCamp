32f1de77f21f477e3fbceab4236e02c649b437ec
VicMontanez <v_montanez@hotmail.com>
2019-07-15 18:09:49 -0400

    lesson5.js

--- Intermediate-Algorithm-Scripting/lesson5.js
+++ Intermediate-Algorithm-Scripting/lesson5.js
@@ -1,11 +1,11 @@
-//lesson 5
+//lesson 5 Spinal Tap Case
 function spinalCase(str) {
-    // "It's such a fine line between stupid, and clever."
-    // --David St. Hubbins
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
   var spaces = /\s+|_+/g;
-  var newStr = str.replace(/([a-z])([A-Z])/g, '$1 $2')
-  
-    return newStr.replace(spaces, '-').toLowerCase()
-  }
-  
-  spinalCase('This Is Spinal Tap');
+  var newStr = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+  return newStr.replace(spaces, "-").toLowerCase();
+}
+
+spinalCase("This Is Spinal Tap");
