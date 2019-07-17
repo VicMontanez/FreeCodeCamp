4242ddb902cdacdde94ff017f168786067b4451a
VicMontanez <v_montanez@hotmail.com>
2019-07-17 19:45:48 -0400

    lesson9

--- Intermediate-Algorithm-Scripting/lesson9.js
+++ Intermediate-Algorithm-Scripting/lesson9.js
@@ -1,16 +1,15 @@
+//Lesson 9
 function fearNotLetter(str) {
-    // split the str and create new array of character's numerical value
-  let numberVal = str.split('').map(function(letter){
+  // split the str and create new array of character's numerical value
+  let numberVal = str.split("").map(function(letter) {
     return letter.charCodeAt();
-    
   });
-  for (let i = 0; i < numberVal.length; i++){
-    if (numberVal[i+1] - numberVal[i] > 1){
-      return String.fromCharCode(numberVal[i]+1);
+  for (let i = 0; i < numberVal.length; i++) {
+    if (numberVal[i + 1] - numberVal[i] > 1) {
+      return String.fromCharCode(numberVal[i] + 1);
     }
   }
-   return undefined;
-  }
-  
-  
-  fearNotLetter("abce");
+  return undefined;
+}
+
+fearNotLetter("abce");
