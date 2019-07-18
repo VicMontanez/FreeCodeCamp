7cf06311ac55a3509c5baa4db61daa8ae56dabb9
VicMontanez <v_montanez@hotmail.com>
2019-07-18 19:31:39 -0400

    lesson10.js

---
+++ Intermediate-Algorithm-Scripting/lesson10.js
@@ -0,0 +1,18 @@
+function uniteUnique(arr) {
+  var args = Array.prototype.slice.call(arguments);
+  var newArr = [];
+
+  args
+    .reduce(function(a, b) {
+      return a.concat(b);
+    })
+    .forEach(function(item) {
+      if (newArr.indexOf(item) < 0) {
+        newArr.push(item);
+      }
+    });
+
+  return newArr;
+}
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
