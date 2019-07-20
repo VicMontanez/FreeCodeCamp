8f33bf53fe6f93c91a537af2400f7d9a75e09e62
VicMontanez <v_montanez@hotmail.com>
2019-07-20 13:52:28 -0400

    lesson11

---
+++ Intermediate-Algorithm-Scripting/lesson11.js
@@ -0,0 +1,18 @@
+function convertHTML(str) {
+  // &colon;&rpar;
+  let htmlCharatcers = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  return str
+    .split("")
+    .map(function(char) {
+      return htmlCharatcers[char] || char;
+    })
+    .join("");
+}
+
+convertHTML("Dolce & Gabbana");
