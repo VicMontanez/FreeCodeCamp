ab42d57f81029bdcc8b4a282a1a41305083a2c72
VicMontanez <v_montanez@hotmail.com>
2019-07-30 17:48:05 -0400

    lesson12

--- Intermediate-Algorithm-Scripting/lesson12.js
+++ Intermediate-Algorithm-Scripting/lesson12.js
@@ -1,1 +1,23 @@
 //Lesson 12
+function sumFibs(num) {
+  Array.prototype.last = function() {
+    return this[this.length - 1];
+  };
+  Array.prototype.secondToLast = function() {
+    return this[this.length - 2];
+  };
+  var newArr = [1, 1];
+
+  while (newArr.secondToLast() + newArr.last() <= num) {
+    newArr.push(newArr.secondToLast() + newArr.last());
+  }
+  return newArr
+    .filter(function(num) {
+      return num % 2 !== 0;
+    })
+    .reduce(function(a, b) {
+      return a + b;
+    });
+}
+
+sumFibs(4);
