5963a0d1180bccaa1d16520153f7446ce72a1817
VicMontanez <v_montanez@hotmail.com>
2019-10-01 23:25:41 -0400

    lesson 6

--- Basic-Algorithm-Scripting/lesson6.js
+++ Basic-Algorithm-Scripting/lesson6.js
@@ -1,9 +1,6 @@
 //Lesson 6
 function confirmEnding(str, target) {
-
-  
   return str.slice(str.length - target.length) === target;
 }
 
-
-confirmEnding("Bastian", "n");
+confirmEnding("Congratulation", "on");
