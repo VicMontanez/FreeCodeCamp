371a3d7a5fede280bd2284fc535d095e1fa1b0bd
VicMontanez <v_montanez@hotmail.com>
2019-10-04 21:35:08 -0400

    lesson11

--- Basic-Algorithm-Scripting/lesson11.js
+++ Basic-Algorithm-Scripting/lesson11.js
@@ -1,7 +1,7 @@
 //Lesson 11 Title case a sentence
 function titleCase(str) {
   return;
-  str.toLowerCase().replace(/(^|\s)\S/g, words => words.toUpperCase());
+  str.toLowerCase().replace(/(^|\s)\S/g, word => word.toUpperCase());
 }
 
 titleCase("I'm a little tea pot");
