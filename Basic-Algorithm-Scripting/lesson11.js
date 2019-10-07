095967287b3e88035ebc28bf5b05d8b393a632dd
VicMontanez <v_montanez@hotmail.com>
2019-10-06 23:23:42 -0400

    lesson 11

--- Basic-Algorithm-Scripting/lesson11.js
+++ Basic-Algorithm-Scripting/lesson11.js
@@ -1,9 +1,9 @@
 function titleCase(str) {
   var lowCase = str.toLowerCase().split(" ");
-  //console.log(lowCase);
+  console.log(lowCase);
   for (var i = 0; i < lowCase.length; i++) {
     lowCase[i] = lowCase[i][0].toUpperCase() + lowCase[i].slice(1);
-    //console.log(lowCase)
+    console.log(lowCase);
   }
 
   return lowCase.join(" ");
