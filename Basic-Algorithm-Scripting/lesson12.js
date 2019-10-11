36e23e5aa72b037f998a03dc71d117d25d6c08fa
VicMontanez <v_montanez@hotmail.com>
2019-10-11 14:25:14 -0400

    lesson12

--- Basic-Algorithm-Scripting/lesson12.js
+++ Basic-Algorithm-Scripting/lesson12.js
@@ -1,13 +1,28 @@
 //Lesson 12 Slice and Splice
 function frankenSplice(arr1, arr2, n) {
-    // It's alive. It's alive!
-    var newArr = [...arr2]
-    newArr.splice(n, 0, ...arr1)
-   
-  console.log(arr2)
-   return newArr
-  
-    ;
-  }
-  
-  frankenSplice([1, 2, 3], [4, 5, 6], 1);
+  // It's alive. It's alive!
+  var newArr = [...arr2];
+  newArr.splice(n, 0, ...arr1);
+
+  console.log(arr2);
+  return newArr;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+// function frankenSplice(arr1, arr2, n) {
+// It's alive. It's alive!
+
+// Secondary Solution //
+//   var newArray = arr2.slice(0);
+
+//   for (var i =0; i < arr1.length; i++) {
+
+//       newArray.splice(n,0,arr1[i])
+//       n += 1
+//   }
+
+//   return newArray;
+// }
+
+// frankenSplice([1, 2, 3], [4, 5, 6], 1);
