0ca1c599050f17778afabe055695b15c0c1c6a7e
VicMontanez <v_montanez@hotmail.com>
2019-10-15 21:40:12 -0400

    fareEstimator

--- Basic-Algorithm-Scripting/fareEstimator.js
+++ Basic-Algorithm-Scripting/fareEstimator.js
@@ -1,1 +1,27 @@
-//Repeat String Algorithm
+function fareEstimator(
+  ride_time,
+  ride_distance,
+  cost_per_minute,
+  cost_per_mile
+) {
+  // var total1 = [];
+  // var total2 =[];
+  var sum = [];
+
+  for (var i = 0; i < cost_per_minute.length; i++) {
+    var totals =
+      cost_per_minute[i] * ride_time + cost_per_mile[i] * ride_distance;
+    sum.push(totals);
+    console.log(totals);
+  }
+  return sum;
+  //     for (var i = 0; i < cost_per_mile.length; i++){
+  //         total2.push(ride_distance * cost_per_mile[i])
+
+  //     }
+  // for (var i = 0; i < total1.length; i++) {
+  //     for (var j = 0; j < total2.length; j++) {
+  //         sum.push(total1[i] + total2[j])
+  //     }
+  // }
+}
