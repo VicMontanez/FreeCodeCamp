d12433f4387488a251076bc31d2f9fb426b9320c
VicMontanez <v_montanez@hotmail.com>
2019-11-04 19:08:32 -0500

    added problem description

---
+++ FCC-T/spinalTapCase.js
@@ -0,0 +1,12 @@
+//Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  var regex = /\s|_/g;
+  var lowerCase = str.toLowerCase();
+  var newStr = lowerCase.replace(regex, "-");
+  return newStr;
+}
+
+spinalCase("This Is Spinal Tap");
