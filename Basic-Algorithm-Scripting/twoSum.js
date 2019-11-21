44d3c191c77f60b3b477e6953c45912c2938d164
VicMontanez <v_montanez@hotmail.com>
2019-11-20 19:30:55 -0500

    twoSum.js

--- Basic-Algorithm-Scripting/twoSum.js
+++ Basic-Algorithm-Scripting/twoSum.js
@@ -4,14 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    var twoNum = []
-    for (var i = 0; i < nums.length; i++) {
-        for (var j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-            twoNum.push(i,j)
-            } 
-        }
+  var twoNum = [];
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        twoNum.push(i, j);
+      }
+    }
   }
-    
-    return twoNum
+
+  return twoNum;
 };
