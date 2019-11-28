fdd4beb4786c621fa8038b884427521e02897ba4
VicMontanez <v_montanez@hotmail.com>
2019-11-27 19:08:03 -0500

    repeatAString.js

---
+++ FCC-T/repeatAString.js
@@ -0,0 +1,16 @@
+// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+
+  var strFull = "";
+  if (num < 0) {
+    return "";
+  }
+  for (var i = 0; i < num; i++) {
+    strFull = str.concat(strFull);
+  }
+  return strFull;
+}
+
+repeatStringNumTimes("abc", 3);
