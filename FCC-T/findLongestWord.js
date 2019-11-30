5795f09f849b30cc6f6ef87f9c8e3af98b22ffe7
VicMontanez <v_montanez@hotmail.com>
2019-11-29 23:59:14 -0500

    added solution

--- FCC-T/findLongestWord.js
+++ FCC-T/findLongestWord.js
@@ -1,3 +1,16 @@
 // Return the length of the longest word in the provided sentence.
 
 // Your response should be a number.
+
+function findLongestWordLength(str) {
+  var words = str.split(" ");
+  var wordLength = 0;
+
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > wordLength) {
+      wordLength = words[i].length;
+    }
+  }
+
+  return wordLength;
+}
