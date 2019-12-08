5c1cd0b79b723b2a90908a00668477d5138a30eb
VicMontanez <v_montanez@hotmail.com>
2019-12-08 18:51:52 -0500

    added code

--- FCC-T/truncateAString.js
+++ FCC-T/truncateAString.js
@@ -1,3 +1,10 @@
 //Truncate a String
 
 // Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  return num < str.length ? str.substr(0, num) + "..." : str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
