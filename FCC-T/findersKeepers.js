80586615250de7e1ddc4b1b66dacc2a3a95c8d8c
VicMontanez <v_montanez@hotmail.com>
2019-12-14 18:53:09 -0500

    added code

--- FCC-T/findersKeepers.js
+++ FCC-T/findersKeepers.js
@@ -1,1 +1,12 @@
 //Finders Keepers
+function findElement(arr, func) {
+  let num = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    if (func(arr[i]) == true) {
+      return arr[i];
+    }
+  }
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
