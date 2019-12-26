f0f031fdbdea23e3c88e9e8881347e177517f6b3
VicMontanez <v_montanez@hotmail.com>
2019-12-25 21:24:40 -0400

    added code

--- Basic-Algorithm-Scripting/checkPalindrome.js
+++ Basic-Algorithm-Scripting/checkPalindrome.js
@@ -1,1 +1,23 @@
 //Check Palindrome -- Code Signal
+function checkPalindrome(inputString) {
+  var sentence = inputString;
+  var backwards = "";
+
+  for (var i = 0; i < sentence.length; i++) {
+    backwards = inputString
+      .split("")
+      .reverse()
+      .join("");
+  }
+  if (backwards === sentence) {
+    console.log(`Words are palindrome!`);
+    return true;
+  } else {
+    console.log(`Words are NOT palindrome!`);
+    return false;
+  }
+}
+
+checkPalindrome("aabaa");
+checkPalindrome("abac");
+checkPalindrome("a");
